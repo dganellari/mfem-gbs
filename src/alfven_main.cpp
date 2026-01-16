0897037db941b437f26fa9e0a64e2c8280c2b86c
gsavva <giannis.savva@epfl.ch>
2026-01-16 14:44:11 +0100

    Use fully qualified names
    to distinguish MFEM constructs and reduce ambiguities

--- src/alfven_main.cpp
+++ src/alfven_main.cpp
@@ -5,14 +5,12 @@
 #include <fstream>
 #include <chrono>
 #include <algorithm>
-
-using namespace std;
-using namespace mfem;
+#include <iomanip>
 
 int main(int argc, char *argv[])
 {
     // Start timer
-    auto start = chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
     // Parameters (default values)
     int ref_lvls = 2;
@@ -21,66 +19,66 @@ int main(int argc, char *argv[])
     double dt = tmax / Nt;
     int order = 1;
     double tol = 1e-14;
-    string mesh_file = string(DATA_DIR) + "/ref-cube.mesh";
+    std::string mesh_file = std::string(DATA_DIR) + "/ref-cube.mesh";
 //  double dt_over_two = dt / 2.0;
     int iter = 1000;
     const char* path_save = "./out/classic/";
 
     // Parse command-line arguments
-    OptionsParser args(argc, argv);
+    mfem::OptionsParser args(argc, argv);
     args.AddOption(&ref_lvls, "-r", "--refine", "Number of refinements.");
     args.AddOption(&Nt, "-nt", "--ntsteps", "Number of timesteps.");
     args.Parse();
 
     if (!args.Good())
     {
-        args.PrintUsage(cout);
+        args.PrintUsage(std::cout);
         return 1;
     }
-    args.PrintOptions(cout); // Print all options used
+    args.PrintOptions(std::cout); // Print all options used
 
     dt = tmax / Nt;
 
     // Load mesh
     const char *mesh_file_cstr = mesh_file.c_str();
-    Mesh mesh(mesh_file_cstr, 1, 1);
+    mfem::Mesh mesh(mesh_file_cstr, 1, 1);
     int dim = mesh.Dimension();
-    cout << "Mesh Dimension : " << dim << endl;
+    std::cout << "Mesh Dimension : " << dim << std::endl;
 
     // Refine mesh
     for (int i = 0; i < ref_lvls; i++) {
-        cout << "Refinement: " << i + 1 << endl;
+        std::cout << "Refinement: " << i + 1 << std::endl;
         mesh.UniformRefinement();
     }
 
     // Print mesh info
-    cout << "\nNumber of elements : " << mesh.GetNE() << endl;
-    cout <<   "Number of vertices : " << mesh.GetNV() << endl;
+    std::cout << "\nNumber of elements : " << mesh.GetNE() << std::endl;
+    std::cout <<   "Number of vertices : " << mesh.GetNV() << std::endl;
 
     // FE spaces
-    H1_FECollection fec_CG(order, dim);
-    FiniteElementSpace CG_u(&mesh, &fec_CG);
-    FiniteElementSpace CG_p(&mesh, &fec_CG);
+    mfem::H1_FECollection fec_CG(order, dim);
+    mfem::FiniteElementSpace CG_u(&mesh, &fec_CG);
+    mfem::FiniteElementSpace CG_p(&mesh, &fec_CG);
 
-    cout << "Number of u unknowns: " << CG_u.GetTrueVSize() << endl;
-    cout << "Number of p unknowns: " << CG_p.GetTrueVSize() << endl;
+    std::cout << "Number of u unknowns: " << CG_u.GetTrueVSize() << std::endl;
+    std::cout << "Number of p unknowns: " << CG_p.GetTrueVSize() << std::endl;
 
     // unknown grid functions
-    GridFunction u(&CG_u);
-    GridFunction p(&CG_p);
+    mfem::GridFunction u(&CG_u);
+    mfem::GridFunction p(&CG_p);
 
     // Initial conditions
-    FunctionCoefficient u_0_coeff(u_0);
-    FunctionCoefficient p_0_coeff(p_0);
+    mfem::FunctionCoefficient u_0_coeff(u_0);
+    mfem::FunctionCoefficient p_0_coeff(p_0);
     u.ProjectCoefficient(u_0_coeff);
     p.ProjectCoefficient(p_0_coeff);
 
     // Export to ParaView
-    ParaViewDataCollection *pd = new ParaViewDataCollection(path_save, &mesh);
+    mfem::ParaViewDataCollection *pd = new mfem::ParaViewDataCollection(path_save, &mesh);
     pd->RegisterField("u", &u);
     pd->RegisterField("p", &p);
     pd->SetLevelsOfDetail(order);
-    pd->SetDataFormat(VTKFormat::BINARY);
+    pd->SetDataFormat(mfem::VTKFormat::BINARY);
     pd->SetHighOrderOutput(true);
     int Nit = 0;
     pd->SetCycle(Nit);
@@ -88,40 +86,40 @@ int main(int argc, char *argv[])
     pd->Save();
 
     // Old time step values
-    Vector u_old(u.Size());
-    Vector p_old(p.Size());
+    mfem::Vector u_old(u.Size());
+    mfem::Vector p_old(p.Size());
     u_old = 0.0;
     p_old = 0.0;
 
     // System size (u + p)
     int ssize = u.Size() + p.Size();
-    cout << "size: " << ssize << endl << endl;
+    std::cout << "size: " << ssize << std::endl << std::endl;
 
     // Vector x: the "full" one (not the tdof one)
-    Vector x(ssize);
+    mfem::Vector x(ssize);
     x.SetVector(u, 0);
     x.SetVector(p, u.Size());
 
     // Identify dofs of u and p
-    Array<int> u_dofs(u.Size());
-    Array<int> p_dofs(p.Size());
-    iota(&u_dofs[0], &u_dofs[u.Size()], 0);
-    iota(&p_dofs[0], &p_dofs[p.Size()], u.Size());
+    mfem::Array<int> u_dofs(u.Size());
+    mfem::Array<int> p_dofs(p.Size());
+    std::iota(&u_dofs[0], &u_dofs[u.Size()], 0);
+    std::iota(&p_dofs[0], &p_dofs[p.Size()], u.Size());
 
     // Create Alfven operator
-    AlfvenOperator oper(CG_u, CG_p, dt);
+    mfem::AlfvenOperator oper(CG_u, CG_p, dt);
 
     // RHS vector
-    Vector b(ssize);
+    mfem::Vector b(ssize);
 
     // Vectors for constrained system (with BC)
-    Vector X(ssize);
-    Vector B(ssize);
-    Operator *A_constrained = nullptr;
+    mfem::Vector X(ssize);
+    mfem::Vector B(ssize);
+    mfem::Operator *A_constrained = nullptr;
 
     // Time loop
     double t = 0.0;
-    real_t energy;
+    mfem::real_t energy;
     int jj = 0;
 
     for (t = dt; t < tmax + dt; t += dt) {
@@ -148,7 +146,7 @@ int main(int argc, char *argv[])
 
         // Compute energy
         energy = oper.ComputeEnergy(u_old, p_old);
-        cout << "step:\t" << jj << "\tt = " << t << "\tenergy = " << energy << endl;
+        std::cout << "step:\t" << jj << "\tt = " << t << "\tenergy = " << energy << std::endl;
 
         // Export data to ParaView
         Nit++;
@@ -160,19 +158,19 @@ int main(int argc, char *argv[])
     // Compute errors
     // u:         FE solution 
     // u_0_coeff: reference/exact solution 
-    real_t up_err = u.ComputeL2Error(u_0_coeff);
-    cout << "u   L2 error " << up_err << endl;
+    mfem::real_t up_err = u.ComputeL2Error(u_0_coeff);
+    std::cout << "u   L2 error " << std::scientific << std::setprecision(16) << up_err << std::endl;
 
-    real_t p_err = p.ComputeL2Error(p_0_coeff);
-    cout << "phi L2 error " << p_err << endl;
+    mfem::real_t p_err = p.ComputeL2Error(p_0_coeff);
+    std::cout << "phi L2 error " << std::scientific << std::setprecision(16) << p_err << std::endl;
 
     // Free memory
     delete pd;
 
     // Timer
-    auto end = chrono::high_resolution_clock::now();
-    chrono::duration<double> elapsed = end - start;
-    cout << "Elapsed time: " << elapsed.count() << " seconds\n";
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
 
     return 0;
 }
